ce383cb0b06b59db448afeacca8f08e2a6c2f835
Ghanwa-Yousaf <120472241+Ghanwa-Yousaf@users.noreply.github.com>
2024-11-14 11:21:33 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,34 @@
-import React from 'react';
-import DigitalArtGallery from './digitalArtGallery';
-import image1 from './IMAGES/blooming steps.png';
-import image2 from './IMAGES/whispers of twilight.png';
-import 'bulma/css/bulma.min.css';
+import React from "react";
+import DigitalArtGallery from "./digitalArtGallery";
+import image1 from "./IMAGES/blooming steps.png";
+import image2 from "./IMAGES/whispers of twilight.png";
+import styles from "./index.css";
 
 function Demo(props) {
   return (
-    <div>
-      {/* Navbar */}
-      <nav className= "navbar" style={{ backgroundColor: '#92c3a5' , fontFamily: "'Pacifico', cursive" }} role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item has-text-white is-size-3 ">
-            Digital Library
-          </a>
-        </div>
-      </nav>
-
-      
-      <section
-        className="section is-flex is-justify-content-center is-align-items-center"
-        style={{ height: '70vh' }}
-      >
-        <div className="container" style={{ maxWidth: '900px' , fontFamily: "'Pacifico', cursive" ,textAlign: 'center' }}>
-          <div className="columns is-centered">
-            <div className="column is-3">
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image is-3by6">
-                    <DigitalArtGallery image={image1} />
-                  </figure>
-                </div>
-                <div className="media-content">
-                  <p className="is-size-5 has-text-weight-bold ">
-                    <DigitalArtGallery title="Blooming Steps" />
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="column is-3">
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image is-3by6">
-                    <DigitalArtGallery image={image2} />
-                  </figure>
-                </div>
-                <div className="media-content">
-                  <p className="is-size-5 has-text-weight-bold ">
-                    <DigitalArtGallery title="Whispers of Twilight" />
-                  </p>
-                </div>
-              </div>
-            </div>
+    <>
+      <div className="navbar">
+        <a className="name">Digital Library</a>
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="card-image">
+            <DigitalArtGallery image={image1} />
+          </div>
+          <div className="media-content">
+            <DigitalArtGallery title="Blooming Steps" />
           </div>
         </div>
-      </section>
-    </div>
+        <div className="card">
+          <div className="card-image">
+            <DigitalArtGallery image={image2} />
+          </div>
+          <div className="media-content">
+            <DigitalArtGallery title="Whispers of Twilight" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/digitalArtGallery.jsx
+++ src/digitalArtGallery.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./index.css";
 
 export const DigitalArtGallery = (props) => {
   return (
     <div>
-         <img src={props.image} />
-         <h2>{props.title}</h2>
-    </div>  
-
+      <img src={props.image} />
+      <h2>{props.title}</h2>
+    </div>
   );
-}
- export default DigitalArtGallery
-
-
-
-
-
- 
+};
+export default DigitalArtGallery;
